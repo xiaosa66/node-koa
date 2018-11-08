36a83e12af6071db27d59bbe89d6c5deb765e7d0
xiaosa <liuxs@dayukeji.xin>
2018-11-08 14:38:03 +0800

    centOS不能运行  试试改一下回车模式

--- app/models/user.js
+++ app/models/user.js
@@ -1,123 +1,1 @@
-'use strict'
-
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema;
-
-/**
- * 定义一个模式(相当于传统意义的表结构)
- * 每个模式映射mongoDB的一个集合，
- * 它定义（只是定义，不是实现）这个集合里面文档的结构，就是定义这个文档有什么字段，字段类型是什么，字段默认值是什么等。
- * 除了定义结构外，还定义文档的实例方法，静态模型方法，复合索引，中间件等
- * @type {mongoose}
- */
-var UserSchema = new Schema({
-	phoneNumber: {
-    unique: true,
-    type: String
-  },
-  areaCode: String,
-  verifyCode: String,
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  accessToken: String,
-  nickname: String,
-  gender: String,
-  breed: String,
-  age: String,
-  avatar: String,
-  meta: {
-    createAt: {
-      type: Date,
-      dafault: Date.now()
-    },
-    updateAt: {
-      type: Date,
-      dafault: Date.now()
-    }
-  }
-})
-var IndexChildSchema = new Schema({
-        hp_title:{type:String},
-    hp_author:{type:String},
-    hp_content:{type:String},
-    hp_makettime:{type:String},
-    meta: {
-      createAt: {
-        type: Date,
-        dafault: Date.now()
-      },
-      updateAt: {
-        type: Date,
-        dafault: Date.now()
-      }
-    }
-    })
-var IndexSchema = new Schema({
-	id: {
-    unique: true,
-    type: String
-  },
-  vols:[IndexChildSchema],
-  meta: {
-    createAt: {
-      type: Date,
-      dafault: Date.now()
-    },
-    updateAt: {
-      type: Date,
-      dafault: Date.now()
-    }
-  }
-})
-
-
-// Defines a pre hook for the document.
-UserSchema.pre('save', function(next) {
-  if (this.isNew) {
-    this.meta.createAt = this.meta.updateAt = Date.now()
-  }
-  else {
-    this.meta.updateAt = Date.now()
-  }
-  next()
-})
-IndexSchema.pre('save', function(next) {
-  if (this.isNew) {
-    this.meta.createAt = this.meta.updateAt = Date.now()
-  }
-  else {
-    this.meta.updateAt = Date.now()
-  }
-  next()
-})
-IndexChildSchema.pre('save', function(next) {
-  if (this.isNew) {
-    this.meta.createAt = this.meta.updateAt = Date.now()
-  }
-  else {
-    this.meta.updateAt = Date.now()
-  }
-  next()
-})
-
-
-/**
- * 定义模型User
- * 模型用来实现我们定义的模式，调用mongoose.model来编译Schema得到Model
- * @type {[type]}
- */
-// 参数User 数据库中的集合名称, 不存在会创建.
-var User = mongoose.model('User', UserSchema)
-var IndexChildSchema = mongoose.model('IndexChildSchema', IndexChildSchema)
-var Index = mongoose.model('Index', IndexSchema)
-
-module.exports = User
-module.exports = Index
-module.exports = IndexChildSchema
-
-
-/**
- * nodejs中文社区这篇帖子对mongoose的用法总结的不错：https://cnodejs.org/topic/548e54d157fd3ae46b233502
- */
+'use strict'var mongoose = require('mongoose')var Schema = mongoose.Schema;/*** * 定义一个模式(相当于传统意义的表结构) * 每个模式映射mongoDB的一个集合， * 它定义（只是定义，不是实现）这个集合里面文档的结构，就是定义这个文档有什么字段，字段类型是什么，字段默认值是什么等。 * 除了定义结构外，还定义文档的实例方法，静态模型方法，复合索引，中间件等 * @type {mongoose} */var UserSchema = new Schema({	phoneNumber: {    unique: true,    type: String  },  areaCode: String,  verifyCode: String,  verified: {    type: Boolean,    default: false  },  accessToken: String,  nickname: String,  gender: String,  breed: String,  age: String,  avatar: String,  meta: {    createAt: {      type: Date,      dafault: Date.now()    },    updateAt: {      type: Date,      dafault: Date.now()    }  }})var IndexChildSchema = new Schema({        hp_title:{type:String},    hp_author:{type:String},    hp_content:{type:String},    hp_makettime:{type:String},    meta: {      createAt: {        type: Date,        dafault: Date.now()      },      updateAt: {        type: Date,        dafault: Date.now()      }    }    })var IndexSchema = new Schema({	id: {    unique: true,    type: String  },  vols:[IndexChildSchema],  meta: {    createAt: {      type: Date,      dafault: Date.now()    },    updateAt: {      type: Date,      dafault: Date.now()    }  }})// Defines a pre hook for the document.UserSchema.pre('save', function(next) {  if (this.isNew) {    this.meta.createAt = this.meta.updateAt = Date.now()  }  else {    this.meta.updateAt = Date.now()  }  next()})IndexSchema.pre('save', function(next) {  if (this.isNew) {    this.meta.createAt = this.meta.updateAt = Date.now()  }  else {    this.meta.updateAt = Date.now()  }  next()})IndexChildSchema.pre('save', function(next) {  if (this.isNew) {    this.meta.createAt = this.meta.updateAt = Date.now()  }  else {    this.meta.updateAt = Date.now()  }  next()})/** * 定义模型User * 模型用来实现我们定义的模式，调用mongoose.model来编译Schema得到Model * @type {[type]} */// 参数User 数据库中的集合名称, 不存在会创建.var User = mongoose.model('User', UserSchema)var IndexChildSchema = mongoose.model('IndexChildSchema', IndexChildSchema)var Index = mongoose.model('Index', IndexSchema)module.exports = Usermodule.exports = Indexmodule.exports = IndexChildSchema/** * nodejs中文社区这篇帖子对mongoose的用法总结的不错：https://cnodejs.org/topic/548e54d157fd3ae46b233502 */
